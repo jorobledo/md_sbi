091f8036514d8d5bda857383d754ef5719513706
Jose Robledo <j.robledo@fz-juelich.de>
2024-11-28 20:17:06 +0100

    update sbi workflow

--- sbi_workflow.py
+++ sbi_workflow.py
@@ -1,123 +1,90 @@
 import numpy as np
-import scipy.stats as stats
 import torch
-import pickle
 import matplotlib.pyplot as plt
+import matplotlib as mpl
 
+# sbi
+import sbi.utils as utils
+from sbi.inference import infer
+from sbi.inference import SNPE, simulate_for_sbi
+from sbi.utils.get_nn_models import posterior_nn
+from sbi.analysis import pairplot
+import pickle
+from simulator import run_md
 
 from sbi.utils.user_input_checks import (
     check_sbi_inputs,
     process_prior,
     process_simulator,
 )
-from sbi.utils.get_nn_models import (
-    posterior_nn,
-) 
-from sbi.utils import BoxUniform
-from sbi import utils as utils
-from sbi import analysis as analysis
-from sbi.inference import simulate_for_sbi, SNPE
-from sbi.analysis import pairplot
 
+prior_min = [1e-3, 0.005, 1.6]
+prior_max = [2000.0, 0.05, 8.0]
 
-from simulator import run_md
+prior = utils.torchutils.BoxUniform(
+    low=torch.as_tensor(prior_min),
+    high=torch.as_tensor(prior_max)
+)
 
-def plot_sim(t, x, name=False):
-    for i in range(x.shape[1]):
-            plt.plot(t, x[:, i], '.', label='atom {}'.format(i))
-    plt.xlabel(r'time (s)')
-    plt.ylabel(r'$x$-Position/Å')
-    plt.legend(frameon=False)
-    if name:
-        plt.savefig(f"{name}.png")
-
-if __name__ == "__main__":
-    
-    # load observation
-    with open("observation.pkl", "rb") as pf:
+# load observation
+with open("observation.pkl", "rb") as pf:
         t, observed_pos = pickle.load(pf)
-    
-    # get initial positions
-    x0 = observed_pos[0,:]
-    dt = t[1]-t[0]
-    
-    # plot observation
-    plot_sim(t, observed_pos, "observed_data")
-    
-    # define simulator
-    def simulator(theta):
-        ''' Define a simulator based on the initial positions observed '''
-        temp, epsilon, sigma = theta.numpy()
-        x = run_md(dt, len(t), x0, temp, epsilon, sigma)
-        variance_pos = x.std(axis=0)
-        min_distances = np.array([min(x[:,0]-x[:,1]), min(x[:,0] - x[:,2]), min(x[:,1] - x[:,2])])
-        final_pos = x[-1,:]
-        return np.stack([variance_pos, min_distances, final_pos]).flatten()
-    
-    # # define what will be compared as observation
-    # def summary_statistics(theta):
-    #     ''' calculate summary statistics based on the measured times '''
-    #     x = simulator(theta)
-    #     # delta_pos = x[-1,:] - x[0,:]
-    #     # delta_t = t[-1] - t[0]
-    #     # avg_velocity = delta_pos / delta_t
-    #     # avg_pos = x.mean(axis=0)
-        
-        
 
-    x_o = np.stack([observed_pos.std(axis=0), 
-                    np.array([min(observed_pos[:,0] - observed_pos[:,1]),
-                              min(observed_pos[:,0] - observed_pos[:,2]), 
-                              min(observed_pos[:,1] - observed_pos[:,2])]),
-                    observed_pos[-1,:]]).flatten()
+# get initial positions
+x0 = observed_pos[0,:]
+dt = t[1]-t[0]
     
-    print("summary statistics observed:", x_o)
+def create_x(theta):
+    if theta.ndim == 1:
+        theta = theta[np.newaxis, :]
+    theta = np.array(theta)
+    return run_md(dt, len(t), x0, theta[:,0], theta[:,1], theta[:,2])
     
-    # define prior (temperature, epsilon, sigma)
-    prior = BoxUniform(low = torch.tensor([1e-3, 0.005, 1.6]), high=torch.tensor([2000.0, 0.05, 8.0]))
-    
-    # Check prior, return PyTorch prior.
-    prior, num_parameters, prior_returns_numpy = process_prior(prior)
+theta_o = np.array([600, 0.01, 3.4])
 
-    # Check simulator, returns PyTorch simulator able to simulate batches.
-    simulator = process_simulator(simulator, prior, prior_returns_numpy)
+def get_summary(theta):
+    x = create_x(theta)
+    variance_pos = x.std(axis=0)
+    min_distances = np.array([min(x[:,0]-x[:,1]), min(x[:,0] - x[:,2]), min(x[:,1] - x[:,2])])
+    final_pos = x[-1,:]
+    return np.stack([variance_pos, min_distances, final_pos]).flatten()
 
-    # Consistency check after making ready for sbi.
-    check_sbi_inputs(simulator, prior)
-    
-    torch.manual_seed(123)
-    np.random.seed(100)
-    # theta = prior.sample((10000,))
-    theta, x = simulate_for_sbi(simulator, prior, num_simulations=10000, num_workers=-1)
-    # x = summary_statistics(theta.numpy())
-    # define method for inference, defaults in training a maf
-    density_estimator_build_fun = posterior_nn(
-    model="nsf", hidden_features=60, num_transforms=3
-    )
-    inference = SNPE(prior=prior, density_estimator=density_estimator_build_fun)
-    inference = inference.append_simulations(theta, x)
-    density_estimator = inference.train()
-    posterior = inference.build_posterior(density_estimator)
-    print(posterior) # prints how the posterior was trained
-    
-    samples = posterior.sample((10000,), x=x_o)
-    mean_vals = samples.mean(axis=0).numpy() 
-    print("result", mean_vals)
-    
-    
-    _ = pairplot(samples,
-                figsize=(6, 6),
-                limits=[(0, 1200),(0.001, 0.05),(1.6, 8.0)],
-                upper='kde', diag='kde',
-                labels=[r"Temperature (K)", r"$\epsilon (eV)$", r"$\sigma (\AA)$"])
-    plt.savefig("result_sbi.png", dpi=300)
-    plt.close()
-    
-    plot_sim(t, observed_pos)
-    x_est = simulator(*mean_vals)
-    print("summary statistics estimated", x_est)
+x_truth = create_x(theta_o)
+for i in range(0,3):
+    plt.plot(t, x_truth[:,i], "k", label=f"atom {i}")
 
-    for i in range(x_est.shape[1]):
-        plt.plot(t, x_est[:, i], '.', label=f'estimated {i}')
-    plt.savefig("estimated.png", dpi=300)
-    
+n_samples = 1000
+# theta = prior.sample((n_samples,))
+# x = create_x(theta.numpy())
+prior, num_parameters, prior_returns_numpy = process_prior(prior)
+simulator = process_simulator(create_x, prior, prior_returns_numpy)
+theta, x = simulate_for_sbi(simulator, prior, num_simulations=n_samples)
+
+color = ['blue', 'orange', 'green']
+for i in range(0,3):
+    for j in range(n_samples):
+        plt.plot(t, x[j,:,i], color=color[i], alpha=0.2)
+plt.ylim(-5,15)
+plt.savefig("prueba.png")
+
+x = get_summary(theta.numpy())
+inference = SNPE(prior)
+_ = inference.append_simulations(theta, x).train()
+posterior = inference.build_posterior()
+
+x_o = get_summary(theta_o)
+theta_p = posterior.sample((1000,), x=x_o)
+
+fig, axes = pairplot(
+    theta_p,
+    limits=list(zip(prior_min, prior_max)),
+    ticks=list(zip(prior_min, prior_max)),
+    figsize=(7, 7),
+    labels=["T", "epsilon", "sigma"],
+    points_offdiag={"markersize": 6},
+    points_colors="r",
+    points=theta_o,
+)
+plt.savefig("result_2.png",dpi=300)
+plt.close()
+
