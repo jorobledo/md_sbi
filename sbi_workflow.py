453011c44d0511a491f7fb3440d1d424706bce7f
Jose Robledo <j.robledo@fz-juelich.de>
2024-11-29 16:51:16 +0100

    added gpu compatibility, although no good increase in performance

--- sbi_workflow.py
+++ sbi_workflow.py
@@ -9,6 +9,7 @@ from scipy.signal import find_peaks
 import sbi.utils as utils
 from sbi.inference import SNPE, simulate_for_sbi
 from sbi.analysis import pairplot
+from sbi.neural_nets import posterior_nn
 import pickle
 from simulator import run_md
 
@@ -17,17 +18,34 @@ from sbi.utils.user_input_checks import (
     process_simulator,
 )
 
-# from multiprocessing import Pool, cpu_count
-
+class FlexibleBoxUniform(utils.BoxUniform):
+    def __init__(self, low, high):
+        self.low = low
+        self.high = high
+        self.device = None
+        
+    def to(self, device):
+        self.device = device
+        super().__init__(
+            low=self.low,
+            high=self.high,
+            device=self.device
+        )
+        
 if __name__ == "__main__":
     # theta = (T, sigma, epsilon)
-    prior_min = [1e-3, 0.005, 1.6]
-    prior_max = [2000.0, 0.05, 5.0]
+    prior_min = torch.as_tensor([1e-3, 0.005, 1.6])
+    prior_max = torch.as_tensor([2000.0, 0.05, 5.0])
 
-    prior = utils.torchutils.BoxUniform(
-        low=torch.as_tensor(prior_min), high=torch.as_tensor(prior_max)
+    prior = FlexibleBoxUniform(
+        low=prior_min, high=prior_max
     )
 
+    if torch.cuda.is_available():
+        device = "cuda"
+    else:
+        device = "cpu"
+        
     # load observation
     with open("data/observation_free.pkl", "rb") as pf:
         t, observed_pos = pickle.load(pf)
@@ -63,7 +81,9 @@ if __name__ == "__main__":
     for i in range(0, x_truth.shape[1]):
         plt.plot(t, x_truth[:, i], "k", label=f"atom {i}")
 
-    n_samples = 10000
+    # simulate in the cpu, multiple cores
+    n_samples = 2000
+    prior.to("cpu")
     prior, num_parameters, prior_returns_numpy = process_prior(prior)
     simulate_x = process_simulator(create_x, prior, prior_returns_numpy)
     simulate_summary = process_simulator(create_summary, prior, prior_returns_numpy)
@@ -91,23 +111,35 @@ if __name__ == "__main__":
     elapsed_time = time.time() - start_time
     print(f"Finished summary statistics calculations in {elapsed_time:.2f} seconds")
 
-    inference = SNPE(prior)
+    # train NN
+    # density_estimator_build_fun = torch.nn.DataParallel(posterior_nn(
+    # model="nsf", hidden_features=30, num_transforms=4
+    # ), device_ids=[i for i in range(4)])
+    prior.to(device)
+    # inference = SNPE(prior=prior, density_estimator=density_estimator_build_fun, device=device)
+    inference = SNPE(prior=prior, device=device)
     print("Starting to train nn")
     start_time = time.time()
     density_estimator = inference.append_simulations(
-        theta, x, proposal=prior, exclude_invalid_x=True
-    ).train(training_batch_size=30, learning_rate=0.0001, show_train_summary=True)
+        theta.to(device), x.to(device),
+        proposal=prior, exclude_invalid_x=True
+    ).train(training_batch_size=30,
+            learning_rate=0.0001,
+            show_train_summary=True)
     elapsed_time = time.time() - start_time
     print(f"Finished training nn in {elapsed_time:.2f} seconds")
-    posterior = inference.build_posterior()
-
+    
+    # bring posterior back again to cpu just in case we are in gpu
+    inference._neural_net.to("cpu")
+    posterior = inference.build_posterior(prior=prior.to("cpu"))
+ 
     # generate observed point summary statistics
     final_pos = x_truth[-1, :]
     avg_pairwise_distance = pairwise_distances(x_truth).mean(axis=0)
     num_cp = np.array(
         [len(find_peaks(i)[0]) for i in ((x_truth - x_truth.mean(axis=0)) ** 2).T]
     )
-    x_o = np.concatenate([avg_pairwise_distance, final_pos, num_cp], axis=0)
+    x_o = torch.as_tensor(np.concatenate([avg_pairwise_distance, final_pos, num_cp], axis=0))
 
     theta_p = posterior.sample((10000,), x=x_o)
 
